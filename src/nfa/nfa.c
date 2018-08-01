d30d8a4445db8b58df680f8079ebb9e2df2a9383
issamouu <issamouu69@gmail.com>
2018-08-01 21:58:11 +0100

    Add support for empty-transition, character and concat nfa-fragments

--- src/nfa/nfa.c
+++ src/nfa/nfa.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../linkedlist/linkedlist.h"
 #include "../digraph/digraph.h"
 #include "nfa.h"
 
@@ -8,8 +9,18 @@ NFA new_nfa(int Q){
     NFA nfa;
     nfa.dg = new_diGraph(Q);
     nfa.initial_state = -1;
-    nfa.to_free = new_linkedList();
+    nfa.to_free = malloc(sizeof(char*));
     return nfa;
+}
+
+// creates an nfa from file
+NFA nfa_from_file(char* filename){
+
+}
+
+// returns a copy of an nfa
+NFA copy_nfa(NFA nfa){
+  
 }
 
 // sets the initial state
@@ -48,7 +59,8 @@ void add_transition(NFA *nfa,int q1, int q2,char symbol){
     add_edge(&nfa->dg,q1,q2,_sym);
 
     // need better solution
-    add_node(&nfa->to_free,_sym);
+    nfa->to_free = realloc(nfa->to_free,nfa->dg.E * sizeof(char*) );
+    nfa->to_free[nfa->dg.E - 1] = _sym;
 
 }
 
@@ -72,11 +84,69 @@ static void print_symbol(void* data){
         printf("%c) \t",symbol);
 }
 
+
+// implements the five basic NFA-fragments
+NFA character_fragment(char c){
+    NFA n = new_nfa(2);
+    set_initial_state(&n,0);
+    add_transition(&n,0,1,c);
+    printf("got out ?");
+    return n;
+}
+
+NFA epsilon_fragment(){
+    return character_fragment(epsilon);
+}
+
+// implements concatenation operator(.)
+NFA concat_fragment(NFA n1,NFA n2){
+    int _size = n1.dg.V + n2.dg.V;
+    NFA n = new_nfa(_size);
+
+    linkedList *ptr = n.dg.adj;
+    linkedList *n_ptr = n1.dg.adj;
+
+    for(int i=0; i < n1.dg.V; i++,ptr++,n_ptr++){
+        linkedList n_copy = copy_list(*n_ptr);
+        join_list(ptr,&n_copy);
+    }
+    
+    n_ptr = n2.dg.adj;
+
+    // TOCHANGE : add possibility to create a graph given a start value for the id of vertices
+
+    for(int i=0; i < n2.dg.V; i++,ptr++,n_ptr++){
+        linkedList n_copy = copy_list(*n_ptr);
+        
+        for(int j=0; j < n_ptr->size;j++){
+            void* data = get(n_copy,j);
+            int * w = (int*)data;
+            *w = *w + n1.dg.V;
+        }
+        join_list(ptr,&n_copy);
+    }
+
+    // add an epsilon transition between final state of n1 and initial state of n2
+    add_transition(&n,n1.dg.V - 1,n1.dg.V,epsilon);
+
+    
+    return n;
+}
+
+NFA union_fragment(NFA n1,NFA n2){
+
+}
+
+NFA closure_fragment(NFA n1);
+
+
 // frees an nfa
 void free_nfa(NFA nfa){
-    
+
     // free all allocated symbols (need a better way to do this)
-    free_list(nfa.to_free);
+    for(int i=0; i < nfa.dg.E; i++)
+        free(nfa.to_free[i]);
+    free(nfa.to_free);
 
     // free the directed graph
     free_graph(&nfa.dg);
--- src/nfa/nfa.h
+++ src/nfa/nfa.h
@@ -9,7 +9,7 @@ typedef struct {
     diGraph dg;
     int initial_state;
     int* final_states;
-    linkedList to_free;
+    char** to_free;
 }NFA;
 
 // for epsilon transition
@@ -17,6 +17,12 @@ typedef struct {
 
 // creates an nfa : Q number of states
 NFA new_nfa(int Q);
+
+// creates an nfa from file
+NFA nfa_from_file(char* filename);
+
+// returns a copy of an nfa
+NFA copy_nfa(NFA nfa);
 
 // sets the initial state
 void set_initial_state(NFA *nfa,int qs);
@@ -33,6 +39,13 @@ void add_transition(NFA *nfa,int q1, int q2,char symbol);
 // prints all reachable states for every states
 void print_nfa(NFA nfa);
 static void print_symbol(void* data);
+
+// implements the five basic NFA-fragments
+NFA character_fragment(char c);
+NFA epsilon_fragment();
+NFA concat_fragment(NFA n1,NFA n2);
+NFA union_fragment(NFA n1,NFA n2);
+NFA closure_fragment(NFA n1);
 
 // frees an nfa
 void free_nfa(NFA nfa);
